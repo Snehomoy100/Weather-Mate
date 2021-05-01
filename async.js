ab1f7968be40e8863cd6fd0b6fae8d025a773e3d
snehomoy100 <snehomoy100@gmail.com>
2021-05-02 02:06:49 +0530

    async.js file added

--- async.js
+++ async.js
@@ -4,7 +4,7 @@ setTimeout(() => {
     console.log("2 second timer");
 }, 2000);
 
-// node's non-blocking I/O feature.
+// nodejs's non-blocking I/O feature.
 
 setTimeout(() => {
     console.log("0 seconds timer");
