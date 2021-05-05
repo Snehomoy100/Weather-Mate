d9ffdc6b2a1a9bc21397f1d2b02ea96261cb4185
snehomoy100 <snehomoy100@gmail.com>
2021-05-06 01:25:58 +0530

    updated

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const request = require('request');
 const url = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=89d69971c452b58e9d8c5fa2b6a5b28b'
 
 request({ url: url, json: true }, (error, response) => {
-    // console.log(response);
-    console.log(response.body.cod);
+    console.log(response.body.list.dt);
+    // const temperature = response.body[list][main][temp];
+    // console.log("It is currently " + temperature);
 });
 
