fb8f5301315399283a8f564e277e5a7992257754
snehomoy100 <snehomoy100@gmail.com>
2021-05-08 20:44:11 +0530

    updated

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const request = require('request');
 const url = 'http://api.openweathermap.org/data/2.5/weather?q=London&appid=89d69971c452b58e9d8c5fa2b6a5b28b&units=metric'
 
 request({ url: url, json: true }, (error, response) => {
-    console.log(response.body.main.temp);
-
+    // console.log(response.body.main.temp);
+    console.log(`It's currently ${response.body.main.temp} degrees out. There is a ${response.body.clouds.all}% chance of rain.`);
 });
 
