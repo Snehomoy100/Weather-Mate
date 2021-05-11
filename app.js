96c5d65b481db35163bd34a157ce64ecfd0c23af
snehomoy100 <snehomoy100@gmail.com>
2021-05-12 02:34:01 +0530

    updated

--- app.js
+++ app.js
@@ -18,18 +18,18 @@ request({ url: url, json: true }, (error, response) => {
 
 
 
-// // Geocoding -> Location -> lat/long co-ordinate pair -> weather.
+// Geocoding -> Location -> lat/long co-ordinate pair -> weather.
 
-// const geocodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoic25laG9tb3kxMDAiLCJhIjoiY2tvNjBnaGY5MWhtZzJxbHl0NWdwOGF0dyJ9.Ps-ZNqoVkpQWZ_apcBgCyA&limit=1';
+const geocodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoic25laG9tb3kxMDAiLCJhIjoiY2tvNjBnaGY5MWhtZzJxbHl0NWdwOGF0dyJ9.Ps-ZNqoVkpQWZ_apcBgCyA&limit=1';
  
-// // first element of center array is longtitude & second one is lattitude.
+// first element of center array is longtitude & second one is lattitude.
 
 
-// request({ url: geocodeUrl, json: true }, (error, response) => {
+request({ url: geocodeUrl, json: true }, (error, response) => {
 
-//     const lattitude = response.body.features[0].center[1];
-//     const longtitude = response.body.features[0].center[0];
-//     console.log(lattitude);
-//     console.log(longtitude);
-// })
+    const lattitude = response.body.features[0].center[1];
+    const longtitude = response.body.features[0].center[0];
+    console.log(lattitude);
+    console.log(longtitude);
+})
 
