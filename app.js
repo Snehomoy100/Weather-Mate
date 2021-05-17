77163535f7f2a9df1697e32022a4d4f163a7fe07
snehomoy100 <snehomoy100@gmail.com>
2021-05-17 16:59:48 +0530

    updated

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const request = require('request');
+// const request = require('request');
 
-const url = 'http://api.openweathermap.org/data/2.5/weather?q=London&appid=89d69971c452b58e9d8c5fa2b6a5b28b&units=metric'
+// const url = 'http://api.openweathermap.org/data/2.5/weather?q=London&appid=89d69971c452b58e9d8c5fa2b6a5b28b&units=metric'
 
 
-request({ url: url, json: true }, (error, response) => {
-    if (error){
-        console.log("Unable to connect...!");
-    } else if(response.body.error){
-        console.log("Must be some error over there");
-    } else {
-        console.log(`There is ${response.body.weather[0].description}. It's currently ${response.body.main.temp} degrees out. There is a ${response.body.clouds.all}% chance of rain out there.`);
-    }
-});
+// request({ url: url, json: true }, (error, response) => {
+//     if (error){
+//         console.log("Unable to connect...!");
+//     } else if(response.body.error){
+//         console.log("Must be some error over there");
+//     } else {
+//         console.log(`There is ${response.body.weather[0].description}. It's currently ${response.body.main.temp} degrees out. There is a ${response.body.clouds.all}% chance of rain out there.`);
+//     }
+// });
 
 
 
@@ -29,7 +29,12 @@ request({ url: geocodeUrl, json: true }, (error, response) => {
 
     const lattitude = response.body.features[0].center[1];
     const longtitude = response.body.features[0].center[0];
-    console.log(lattitude);
-    console.log(longtitude);
+    if (error) {
+        console.log("Unable to Connect...");
+    } else if(response.body.error){
+        console.log("Something is going wrong at your end...");
+    } else {
+        console.log(lattitude, longtitude);
+    }
 })
 
