727d264f4f12ed5afba7a7fbb15efee6992945c1
snehomoy100 <snehomoy100@gmail.com>
2021-05-17 17:01:48 +0530

    updated

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// const request = require('request');
+const request = require('request');
 
 // const url = 'http://api.openweathermap.org/data/2.5/weather?q=London&appid=89d69971c452b58e9d8c5fa2b6a5b28b&units=metric'
 
@@ -37,4 +37,3 @@ request({ url: geocodeUrl, json: true }, (error, response) => {
         console.log(lattitude, longtitude);
     }
 })
-
