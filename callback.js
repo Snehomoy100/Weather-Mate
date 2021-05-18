741abe3ba43d39e7efee2a98d7772a5749b18c85
snehomoy100 <snehomoy100@gmail.com>
2021-05-18 12:43:27 +0530

    updated

--- callback.js
+++ callback.js
@@ -28,6 +28,8 @@
 // });
 
 
+
+
 //
 // Goal: Mess around with the callback pattern
 //
@@ -35,6 +37,7 @@
 // 2. Use setTimeout to simulate a 2 second delay
 // 3. After 2 seconds are up, call the callback function with the sum
 // 4. Test your work!
+
 
 const add = (a, b, callback) => {
     setTimeout(() => {
@@ -46,4 +49,4 @@ const add = (a, b, callback) => {
 
 add(1, 4, (sum) => {
     console.log(sum) // Should print: 5
-})
+});
