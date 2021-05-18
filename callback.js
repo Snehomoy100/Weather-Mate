75ee8877cd816d89e91d67572467765c8865b5cc
snehomoy100 <snehomoy100@gmail.com>
2021-05-18 12:36:00 +0530

    updated

--- callback.js
+++ callback.js
@@ -1,12 +1,26 @@
-setTimeout(() => {
-    console.log("2 seconds are up!");
-}, 2000);
+// setTimeout(() => {
+//     console.log("2 seconds are up!");
+// }, 2000);
 
 
 
-const names = ['Snehomoy', 'Rupomoy', 'Soumya'];
+// const names = ['Snehomoy', 'Rupomoy', 'Soumya'];
 
-const eachName = names.filter((name) => {
-    console.log(name.length <= 4);
-});
+// const eachName = names.filter((name) => {
+//     console.log(name.length <= 4);
+// });
 
+
+const geoCoding = (address, calback) => {
+    const data = {
+        lattitude: 0,
+        longitude: 0
+    };
+
+    return data;
+}
+
+
+const data = geoCoding('Mumbai');
+
+console.log(data);
