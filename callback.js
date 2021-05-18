98bfb944457497a8363f3c3d70c10451723866f6
snehomoy100 <snehomoy100@gmail.com>
2021-05-18 12:42:29 +0530

    updated

--- callback.js
+++ callback.js
@@ -27,3 +27,23 @@
 //     console.log(data);
 // });
 
+
+//
+// Goal: Mess around with the callback pattern
+//
+// 1. Define an add function that accepts the correct arguments
+// 2. Use setTimeout to simulate a 2 second delay
+// 3. After 2 seconds are up, call the callback function with the sum
+// 4. Test your work!
+
+const add = (a, b, callback) => {
+    setTimeout(() => {
+        const sum = a + b;
+
+        callback(sum);
+    }, 2000);
+};
+
+add(1, 4, (sum) => {
+    console.log(sum) // Should print: 5
+})
