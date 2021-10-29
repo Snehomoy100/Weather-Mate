a13d9522f1b5498085eff42f3800903cb7c20ca4
snehomoy100 <snehomoy100@gmail.com>
2021-10-29 20:51:41 +0530

    app.js file updated with the API Key & HTTP request feature

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-// handling http requests in nodejs
+// handling http requests in nodejs using request library
 
 const request = require('request');
 
 const url = 'https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=44350a15f38473661ce8cc0150e8b306';
 
 request({url: url}, (error, response) => {
-    console.log(response);
+    const data = JSON.parse(response.body);
+    console.log(data.cod);
 })
